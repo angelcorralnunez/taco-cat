7115e39427264c682192b9c1621129ba5e4e38e3
Angelena Nunez <angelenacorral@gmail.com>
2023-04-16 04:03:07 -0600

    updated app name, description

--- js/site.js
+++ js/site.js
@@ -1,12 +1,38 @@
-function displayMessage() {
-    // represents value in text box
-    let msg = document.getElementById('message').value;
-    // alert(msg);
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+// App Controller / Entry Point
+function getValues() {
+    // get the users input
+    // decide what to do with it
+    let userInput = document.getElementById('message').value;
+    
+    let reversedInput = reverseString(userInput)
+
+    displayString(reversedInput);
 }
+
+// Business Logic
+function reverseString(message) {
+    // take a string, return it in reverse
+    let reversedMessage = ''
+
+    //  reverse the string
+    for (let index = message.length - 1; index >= 0; index = index - 1){
+        reversedMessage += message[index];
+    }
+
+    return reversedMessage;
+}
+// View Function
+function displayString(reversedMessage) {
+    // show the string on the page / assign text input to reversedMessage
+    document.getElementById('msg').textContent = reversedMessage;
+    // you want to remove the class d-none to get the alert to show / display
+    document.getElementById('alert').classList.remove('d-none')
+}
+
+    // Swal.fire(
+    //     {
+    //         backdrop: false,
+    //         title: 'Reverse Rewind',
+    //         text: msg
+    //     }
+    // );
